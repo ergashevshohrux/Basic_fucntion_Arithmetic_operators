091db8248eb93ff1a6631632ed4965c1e54c245d
Shohrux <shohruxergashev1204@gmail.com>
2022-06-20 13:36:28 +0500

    arithmetic

--- bin/arithmetic06.dart
+++ bin/arithmetic06.dart
@@ -3,5 +3,9 @@
 // the two-digit numbers will be given.
 // Find the reverse of the number and return to a variable called ‘answer’
 // return answer
-
-void main() {}
+int func (number) {
+   return number~/10*10+number%10;
+}
+void main() {
+    print (func(50));
+}
