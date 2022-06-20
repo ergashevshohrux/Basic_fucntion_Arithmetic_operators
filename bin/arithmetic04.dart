ad49f935714ec6728a9847b48eac6b0ff08fe0fd
Shohrux <shohruxergashev1204@gmail.com>
2022-06-20 13:16:50 +0500

    arithmetic

--- bin/arithmetic04.dart
+++ bin/arithmetic04.dart
@@ -1,5 +1,9 @@
 // Create a function called multiple.
 // Create function arguments a and b of type int.
 // Return the multiplication of a and b.
-
-void main() {}
+int multiple (a,b) {
+ return a*b;
+}
+void main() {
+    print (multiple(4, 5));
+}
