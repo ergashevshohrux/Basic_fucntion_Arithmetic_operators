a5b543a10500de8265f88e63f240dd6ebd5b35b1
Shohrux <shohruxergashev1204@gmail.com>
2022-06-20 10:38:11 +0500

    arithmetic

--- bin/arithmetic02.dart
+++ bin/arithmetic02.dart
@@ -1,7 +1,9 @@
 // Create a function called divide.
 // Create function arguments a and b of type int.
 // Returns the floor division of a and b.
-
+int func (a,b) {
+    return a%b;
+}
 void main() {
-  
+  print (func(17,12));
 }
